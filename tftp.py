1b319c70982c23bb188d9be0a4cbd57dd0a1ee5c
vercasson <victor.vercasson@etu.u-bordeaux.fr>
2021-04-09 11:26:28 +0200

    SENDTO

--- tftp.py
+++ tftp.py
@@ -32,21 +32,21 @@ def runServer(addr, timeout, thread):
         mode = args[1].decode('ascii') 
 
         sServeur = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # creation d'un socket de reponse
-        sServeur.bind("localhost",33425)
-        sServeur.sendto(b'\x00\x04\x00\x00') # ACK de reponse WRQ
+        sServeur.bind(('',33425))
+        sServeur.sendto(b'\x00\x04\x00\x00',adresse) # ACK de reponse WRQ
 
         # Requete RRQ
         if opcode == 1:
             #ouverture du fichier envoyé
             file = open(filename,'r')
             message = file.readlines()
-            sServeur.sento(b'\x00\x03\x00\x01',message)
+            sServeur.sento(b'\x00\x03\x00\x01'+message,adresse)
         # Requete WRQ
         if opcode == 2:
             dataWRQ,adresseWRQ = sServeur.recvfrom(1500)
 
             #condition data bien recu
-            sServeur.sendto(b'\x00\x04\x00\x01') # ACK de réponse DAT1
+            sServeur.sendto(b'\x00\x04\x00\x01',adresse) # ACK de réponse DAT1
 
 ########################################################################
 #                             CLIENT SIDE                              #
@@ -69,10 +69,11 @@ def put(addr, filename, targetname, blksize, timeout):
     s.sendto(frameWRQ,addr)
 
     # Opening the file to send
-    file = open(filename,'r')
     if targetname != filename:
         file = open(targetname,'r')
-    
+    else:
+        file = open(filename,'r')
+
     # receive first ACK
     data, serverAddr = s.recvfrom(blksize)
     while True:
@@ -90,7 +91,36 @@ def put(addr, filename, targetname, blksize, timeout):
 
 
 def get(addr, filename, targetname, blksize, timeout):
-    # todo
-    pass
+    # Creating the socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Uncomment to set the timeout
+    s.settimeout(timeout)
+
+    # count data packets
+    dataCnt = 1
+
+    # Initializing and sending the write request
+    frameRRQ = b'\x00\x01' + filename.encode() + b'\x00octet\x00'
+    if blksize != 512:
+        frameWRQ += b'blksize\x00' + str(blksize).encode() + b'\x00'
+    s.sendto(frameWRQ,addr)
+
+    # Opening the file to send
+    file = open(filename,'r')
+    if targetname != filename:
+        file = open(targetname,'r')
+    
+    # receive first ACK
+    data, serverAddr = s.recvfrom(blksize)
+    while True:
+        # read from file and send data
+        pakcet = file.read(blksize)
+        encodedPakcet = pakcet.encode()
+        s.sendto(encodedPakcet,addr)
+
+        # receive confirmation
+        data, serverAddr = s.recvfrom(1500)
+
+    s.close()
 
 # EOF
